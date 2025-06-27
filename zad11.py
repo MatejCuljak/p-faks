9e4b57bd3edf61d6124f6033d4d08b18d5624887
Matej Čuljak <matej.culjak404@gmail.com>
2025-06-27 15:44:14 +0200

    add: zad11.py

---
+++ zad11.py
@@ -0,0 +1,6 @@
+#Funkciju iz prethodne zadaće učitati kao funkciju modula u novi program i pozvati je nakon traženja unosa od korisnika.
+from zad1 import ispis_prezimena
+def main():
+    ispis_prezimena()
+if __name__ == "__main__":
+    main()
